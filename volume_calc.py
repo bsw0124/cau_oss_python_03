0800c577655c6ba21c1a1323aa5fb71948fa5fb4
bsw0124 <127831929+bsw0124@users.noreply.github.com>
2023-03-14 19:07:33 +0900

    Create volume_calc.py
    박스의 부피 구하기 과제

---
+++ volume_calc.py
@@ -0,0 +1,9 @@
+width = float(input("가로:"))
+length = float(input("세로:"))
+height = float(input("높이:"))
+volume = width * length * height
+print("가로:",width)
+print("세로:",length)
+print("높이:",height)
+print("박스의 부피는",volume, end='') 
+print("입니다")
